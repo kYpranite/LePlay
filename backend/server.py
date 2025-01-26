c24559ea562a7d2bf8e4bd141631c3b41bda2e20
Ethan <ethanhuang2078@gmail.com>
2025-01-25 23:52:12 -0800

    add cutting video with ffmpeg

--- backend/server.py
+++ backend/server.py
@@ -1,10 +1,11 @@
 
 from flask import Flask, render_template, request, jsonify, flash
-
-import flask_uploads
+import subprocess
+import math
 import os
 from flask_cors import CORS
 from flask_uploads import UploadSet
+import flask_uploads
 
 
 app = Flask(__name__)
@@ -18,16 +19,43 @@ def home():
 VIDEOS = ["mp4"]
 print (VIDEOS)
 videos = UploadSet("videos", VIDEOS)
-app.config["UPLOADED_VIDEOS_DEST"] = "media/"
+app.config["UPLOADED_VIDEOS_DEST"] = "media/unprocessed"
 app.config["SECRET_KEY"] = os.urandom(24)
 flask_uploads.configure_uploads(app, videos)
+    
+def split_video(path, chunk_duration):
+        result = subprocess.run(
+            ['ffprobe', '-v', 'error', '-show_entries', 'format=duration', '-of', 'default=noprint_wrappers=1:nokey=1', path],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True
+        )
+        duration = float(result.stdout.strip())
+        num_chunks = math.ceil(duration / chunk_duration)
+        for i in range(num_chunks):
+            start_time = i * chunk_duration
+            output_file = os.path.join("./media/clips", f"chunk_{i+1}.mp4")
 
+            # FFmpeg command to extract a chunk
+            command = [
+                'ffmpeg', '-i', path, '-ss', str(start_time),
+                '-t', str(chunk_duration), '-c', 'copy', output_file
+            ]
+
+            print(f"Creating chunk {i+1}/{num_chunks}: {output_file}")
+            subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        
+        
+        
+    
+    
 
 @app.route("/api/upload", methods=['GET', 'POST'])
 def upload():
     if request.method == 'POST' and 'video' in request.files:
-        videos.save(request.files['video'])
-        return "Welcome to the Flask server!"
+        fileName = videos.save(request.files['video'])
+        split_video("./media/unprocessed/" + fileName ,20*60)
+        return "Successful!!"
     return
     
 
